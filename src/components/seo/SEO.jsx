b6337b7286b473522a6d53b9d7e87701aa9ba646
Ibro <ibroabdraimov20@gmail.com>
2025-12-03 11:33:19 +0600

    sdfasDF

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -20,22 +20,22 @@ const SEO = ({
   // Дефолтные значения для каждого языка
   const seoDefaults = {
     ru: {
-      siteName: 'Международный колледж IT и бизнеса - Салымбеков Университет',
-      defaultTitle: 'Международный колледж IT и бизнеса | Салымбеков Университет',
-      defaultDescription: 'Международный колледж информационных технологий и бизнеса при Салымбеков Университете. Двойные дипломы, программы на английском языке, партнерство с Lincoln и INTI университетами.',
-      defaultKeywords: 'колледж, IT образование, компьютерная наука, мультимедиа, мобильные приложения, международный диплом, Салымбеков Университет, Lincoln University, INTI, программирование, веб разработка'
+      siteName: 'Салымбеков Колледж | Международный Колледж IT и Бизнеса',
+      defaultTitle: 'Салымбеков Колледж | Колледж Салымбекова | Международный Колледж IT и Бизнеса',
+      defaultDescription: 'Салымбеков Колледж - ведущий международный колледж IT и бизнеса в Кыргызстане. Двойные дипломы с Lincoln и INTI университетами. Колледж Салымбеков Университет.',
+      defaultKeywords: 'Салымбеков колледж, Колледж Салымбекова, Колледж Салымбеков университет, Колледж IT и бизнеса, международный колледж, компьютерная наука, мультимедиа, мобильные приложения, двойной диплом, Lincoln университет, INTI университет, IT образование Кыргызстан, бизнес образование, программирование, веб-разработка, колледж Бишкек, IT колледж Кыргызстан'
     },
     en: {
-      siteName: 'International College of IT and Business - Salymbekov University',
-      defaultTitle: 'International College of IT and Business | Salymbekov University',
-      defaultDescription: 'International College of Information Technology and Business at Salymbekov University. Double degrees, English-taught programs, partnership with Lincoln and INTI universities.',
-      defaultKeywords: 'college, IT education, computer science, multimedia, mobile applications, international degree, Salymbekov University, Lincoln University, INTI, programming, web development'
+      siteName: 'Salymbekov College | International College of IT and Business',
+      defaultTitle: 'Salymbekov College | Salymbekov College | International College of IT and Business',
+      defaultDescription: 'Salymbekov College - leading international college of IT and business in Kyrgyzstan. Double degrees with Lincoln and INTI universities.',
+      defaultKeywords: 'Salymbekov college, Salymbekov college, Salymbekov university college, IT and business college, international college, computer science, multimedia, mobile applications, double degree, Lincoln university, INTI university, IT education Kyrgyzstan, business education, programming, web development'
     },
     kg: {
-      siteName: 'Эларалык IT жана бизнес колледжи - Салымбеков Университети',
-      defaultTitle: 'Эларалык IT жана бизнес колледжи | Салымбеков Университети',
-      defaultDescription: 'Салымбеков Университетиндеги Эларалык информатика технологиялары жана бизнес колледжи. Кош дипломдор, англис тилиндеги программалар, Lincoln жана INTI университеттери менен өнөктөштүк.',
-      defaultKeywords: 'колледж, IT билим берүү, компьютер илими, мультимедиа, мобилдик колдонмолор, эларалык диплом, Салымбеков Университети, Lincoln University, INTI, программалоо, веб иштеп чыгуу'
+      siteName: 'Салымбеков Колледжи | Эларалык IT жана Бизнес Колледжи',
+      defaultTitle: 'Салымбеков Колледжи | Салымбеков Колледжи | Эларалык IT жана Бизнес Колледжи',
+      defaultDescription: 'Салымбеков Колледжи - Кыргызстандагы IT жана бизнес тармагындагы алдыңкы эларалык колледж. Lincoln жана INTI университеттери менен кош дипломдор.',
+      defaultKeywords: 'Салымбеков колледжи, Салымбеков колледжи, Салымбеков университети колледжи, IT жана бизнес колледжи, эларалык колледж, компьютер илими, мультимедиа, мобилдик колдонмолор, кош диплом, Lincoln университети, INTI университети, IT билим берүү Кыргызстан, бизнес билим берүү, программалоо, веб иштеп чыгуу'
     }
   };
 
@@ -88,27 +88,47 @@ const SEO = ({
         {JSON.stringify({
           "@context": "https://schema.org",
           "@type": "EducationalOrganization",
-          "name": defaults.siteName,
-          "description": seoDescription,
+          "name": "Салымбеков Колледж",
+          "alternateName": ["Колледж Салымбекова", "Колледж Салымбеков Университет", "Международный Колледж IT и Бизнеса", "Salymbekov College"],
+          "description": "Ведущий международный колледж информационных технологий и бизнеса в Кыргызстане. Двойные дипломы с Lincoln и INTI университетами.",
           "url": baseUrl,
           "logo": `${baseUrl}/images/logo.png`,
           "image": seoImage,
           "address": {
             "@type": "PostalAddress",
-            "streetAddress": "ул. Токтогула 164/1",
+            "streetAddress": "ул. Малдыбаева 24б",
             "addressLocality": "Бишкек",
+            "addressRegion": "Чуйская область",
+            "postalCode": "720000",
             "addressCountry": "KG"
           },
           "contactPoint": {
             "@type": "ContactPoint",
-            "telephone": "+996 312 545454",
-            "contactType": "customer service",
+            "telephone": "+996-706-998-889",
+            "contactType": "admissions",
             "availableLanguage": ["Russian", "English", "Kyrgyz"]
           },
           "sameAs": [
-            "https://www.facebook.com/salymbekov.university",
-            "https://www.instagram.com/salymbekov_university"
-          ]
+            "https://facebook.com/salymbekovuniversity",
+            "https://instagram.com/salymbekovuniversity",
+            "https://t.me/salymbekovuniversity",
+            "https://wa.me/996706998889"
+          ],
+          "educationalCredentialAwarded": "Двойной диплом",
+          "hasEducationalUse": "Высшее образование",
+          "knowsAbout": [
+            "Компьютерная наука",
+            "Бизнес образование",
+            "IT технологии",
+            "Программирование",
+            "Веб-разработка"
+          ],
+          "areaServed": {
+            "@type": "Country",
+            "name": "Кыргызстан"
+          },
+          "foundingDate": "2020",
+          "numberOfStudents": "500"
         })}
       </script>
     </Helmet>
